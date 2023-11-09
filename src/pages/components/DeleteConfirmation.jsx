e28f79c2631c77554d2d2c51091c3cac2ae91181
zuzannaranuszkiewicz <eaazura@students.eaaa.dk>
2023-11-09 04:27:43 +0100

    entry display and entry edit

--- src/pages/components/DeleteConfirmation.jsx
+++ src/pages/components/DeleteConfirmation.jsx
@@ -10,8 +10,10 @@ export function Modal({ show, onClose, onConfirm }) {
         <div className="moduleContainer">
           <h3>Are You Sure?</h3>
           <p>You won't be able to access this entry again once you delete it.</p>
+          <div style={{display: 'flex', flexDirection: 'row', gap: '1rem'}}>
           <button onClick={onConfirm} className='deleteButton' style={{backgroundColor: 'var(--dailyChallenge)'}}>Delete</button>
-          <button className='deleteButton' onClick={onClose}>Cancel</button>
+          <button className='deleteButton' style={{backgroundColor: 'var(--accColLight)'}} onClick={onClose}>Cancel</button>
+          </div>
         </div>
       </div>
     );
